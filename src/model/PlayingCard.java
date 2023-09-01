0b395bda864acb0032bdfd76d1a1de3f62178228
Archange Mbah Tchategouang <archange.mbah.tchategouang@mni.thm.de>
2023-09-01 21:52:25 +0200

    initial commit

---
+++ src/model/PlayingCard.java
@@ -0,0 +1,26 @@
+package model;
+
+public class PlayingCard {
+   private  Rank rank;
+   private  Suit suit;
+   private  boolean faceUp=false;
+    public PlayingCard(Rank rank, Suit suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+    public Rank getRank(){
+        return this.rank;
+    }
+    public Suit getSuit(){
+        return this.suit;
+    }
+    public boolean isFaceUp(){
+        return this.faceUp;
+    }
+    public void flip(){
+        this.faceUp = !this.faceUp;
+    }
+
+
+    
+}
